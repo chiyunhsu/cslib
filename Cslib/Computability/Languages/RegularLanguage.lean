5f53ce57ee9216a9e62fe8fd59e2894b21363304
Brooke Gill <96643991+brooke-gill@users.noreply.github.com>
2026-09-01 14:20:36 -0700

    Folded rw statement into simp_all

--- Cslib/Computability/Languages/RegularLanguage.lean
+++ Cslib/Computability/Languages/RegularLanguage.lean
@@ -492,8 +492,7 @@ lemma splitLast_mem [DecidableEq Symbol] {n : ℕ} {flts : FLTS (Fin n) Symbol}
         -- use `mem_PathSupp_of_neq_splitLast` and `splitLast_eq`
         -- (Brooke do this second)
         apply mem_PathSupp_of_neq_splitLast at hc1
-        rw [splitLast_eq hk (by grind)]
-        simp_all [Accepts, PathSupp]
+        simp_all [Accepts, PathSupp, splitLast_eq]
     · -- The last `k` is equal to `flts.tr i a`
       -- Cannot apply ih
       -- Directly prove the goal from definition
